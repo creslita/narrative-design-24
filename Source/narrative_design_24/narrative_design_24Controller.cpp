1e4ab8600f6f3fe068f0062ee462e011d4c1f6e4
Anno! <79550877+Sir-Anno@users.noreply.github.com>
2024-05-12 16:45:57 +0100

    Player character, controller and game mode

---
+++ Source/narrative_design_24/narrative_design_24Controller.cpp
@@ -0,0 +1,4 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "narrative_design_24Controller.h"
---
+++ Source/narrative_design_24/narrative_design_24Controller.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "narrative_design_24Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NARRATIVE_DESIGN_24_API Anarrative_design_24Controller : public APlayerController
+{
+	GENERATED_BODY()
+};
